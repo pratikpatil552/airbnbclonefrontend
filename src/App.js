4ee901ddb9810e3fdf3de579c946c01a987e4e9b
Pratik Patil <pratikpatilmudalp3@gmail.com>
2024-02-04 16:26:04 +0530

    work for pages like login and register is done task it to complete its endpoint on the next day

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import './App.css';
 import Layout from './Layout';
 import IndexPage from './components/IndexPage';
 import LoginPage from './components/LoginPage';
+import RegisterPage from './components/RegisterPage';
 
 function App() {
   return (
     <Routes>
       <Route path ="/" element={<Layout/>}>
-        <Route path = "/login" element = {<LoginPage/>}/>
         <Route path = "/" element = {<IndexPage/>}/>
+        <Route path = "/login" element = {<LoginPage/>}/>
+        <Route path = "/register" element = {<RegisterPage/>}/>
       </Route>
     </Routes>
   );
--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-const LoginPage = () => {
+const LoginPage = () => { 
   return (
-    <div className='mt-4'>
-    <h1 className='text-4xl text-center'>Login</h1>
-      <form className='max-w-md mx-auto border'>
-        <input type="email" placeholder={"enter your email"}/>
-        <input type="password" placeholder='password' />
-        <button>Login</button>
-      </form>
+    <div className='mt-4 grow flex items-center justify-around'>
+        <div className='mb-64'>
+            <h1 className='text-4xl text-center mb-4'>Login</h1>
+            <form className='max-w-md mx-auto'>
+              <input type="email" placeholder={"enter your email"}/>
+              <input type="password" placeholder='password' />
+              <button className='primary'>Login</button>
+              <div className='text-center py-2 text-gray-500'>
+                Don't have an account yet ? 
+                <Link className='underline text-black' to = {'/register'}>Register now</Link>
+                </div>
+            </form>
+        </div>
     </div>
   )
 }
