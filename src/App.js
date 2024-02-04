7fe50c80876ba60c6a64629ded27bc55412cb54a
Pratik Patil <pratikpatilmudalp3@gmail.com>
2024-02-04 16:04:29 +0530

    project startup and setup of react router is done along started index page and login page also layout is also defined

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import { Routes,Route } from 'react-router-dom';
 import './App.css';
+
+
+import Layout from './Layout';
+import IndexPage from './components/IndexPage';
+import LoginPage from './components/LoginPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path ="/" element={<Layout/>}>
+        <Route path = "/login" element = {<LoginPage/>}/>
+        <Route path = "/" element = {<IndexPage/>}/>
+      </Route>
+    </Routes>
   );
 }
 
---
+++ src/components/IndexPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const IndexPage = () => {
+  return (
+    <div>
+      this is the index page
+    </div>
+  )
+}
+
+export default IndexPage
